d0d8bc413ffe497a6c4c98b83ccaf374468a910a
Nishith Kodiganti <nishithkumar756@gmail.com>
2023-11-04 23:07:50 +0530

    Leet Code Problem 6

---
+++ three_consecutive_odds.py
@@ -0,0 +1,5 @@
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        for i in range(0,len(arr)-2):
+            if arr[i]%2!=0 and arr[i+1]%2!=0 and arr[i+2]%2!=0:
+                return(arr[i:][:3])
